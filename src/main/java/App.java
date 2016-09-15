ed4b808096905c95a641a84b0e103ce9a1005642
GeorginaVanDort <georginavandort@gmail.com>
2016-09-14 19:57:28 -0700

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,24 +4,22 @@ import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 
+//Make sure you have VelocityTemplateEngine file!//
 public class App {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) {//static effects the class as a WHOLE. Not an instance of it//
 
-    staticFileLocation("/public");
+    staticFileLocation("/public"); //declare a static file location//
 
-    String layout = "templates/layout.vtl";
-
+    String layout = "templates/layout.vtl";//Decalre a String called "layout" that euqals the layout file//
+//Creat appropriate resources and templates DIR//
 
     get("/", (request, response) -> {
-
-      Map<String, Object> model = new HashMap<String, Object>();
-
-      model.put("template", "templates/index.vtl");
-
-      return new ModelAndView(model, layout);
-
-    }, new VelocityTemplateEngine());
+      Map<String, Object> model = new HashMap<String, Object>();//Creates a new HashMap//
+      model.put("template", "templates/index.vtl");//Saves the index as the VALUE of "template" KEY//
+      return new ModelAndView(model, layout);//populates the layout using the model//
+    }, new VelocityTemplateEngine()); 
+    //Creates a Root route that loads template.vtl//
 
 
     get("/rectangle", (request, response) -> {
@@ -29,21 +27,20 @@ public class App {
       Map<String, Object> model = new HashMap<String, Object>();
 
       int length = Integer.parseInt(request.queryParams("length"));
-
       int width = Integer.parseInt(request.queryParams("width"));
+      //save the information provided by the user into int length and int width variables//
 
 
-      Rectangle myRectangle = new Rectangle(length, width);
+      Rectangle myRectangle = new Rectangle(length, width);//instantiate a Rectangle object with the length and width variables//
 
-      model.put("myRectangle", myRectangle);
+      model.put("myRectangle", myRectangle);//pass it to our model as a value under the key "myRectangle"//
 
 
       if (myRectangle.isSquare()) {
 
        Cube myCube = new Cube(myRectangle);
 
-       model.put("myCube", myCube);
-
+       model.put("myCube", myCube); // add our new Cube to model//
      }
 
 
